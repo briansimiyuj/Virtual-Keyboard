7481b2200e61edfc95b8d575123b17cf0811daea
briansimiyuj <briansimiyuj@gmail.com>
2020-11-19 14:05:11 +0300

    fixed a bug on text DOM

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const caps = document.querySelector('.caps')
 
 const texts = document.querySelectorAll('.text')
 
+const textsBTN = document.querySelectorAll('.text button')
+
 const buttons = document.querySelectorAll('button')
 
 const textArea = document.querySelector('textArea')
@@ -14,7 +16,7 @@ const space = document.querySelector('.space')
 
 const wide = document.querySelector('#wide')
 
-console.log(caps, texts, buttons, textArea, enter, check, space, wide)
+console.log(caps, texts, buttons, textArea, enter, check, space, wide, textsBTN)
 
 
 
