d3e78e60e9c924ed06ff585c524d7f6135fea5c1
briansimiyuj <briansimiyuj@gmail.com>
2020-11-19 13:28:28 +0300

    Get the DOM elements

---
+++ script.js
@@ -0,0 +1,19 @@
+const caps = document.querySelector('.caps')
+
+const texts = document.querySelectorAll('.text')
+
+const buttons = document.querySelectorAll('button')
+
+const textArea = document.querySelector('textArea')
+
+const enter = document.querySelector('.enter') 
+
+const check = document.querySelector('.check')
+
+const space = document.querySelector('.space')
+
+const wide = document.querySelector('#wide')
+
+console.log(caps, texts, buttons, textArea, enter, check, space, wide)
+
+
